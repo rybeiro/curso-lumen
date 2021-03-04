8f8644593087103b8847f9bb6856d17b0c2c5d8b
Fabio Ribeiro <fabioribeiro@tecnologia.local>
2021-03-03 22:04:07 -0300

    Inicio do curso de Laravel/Lumen

---
+++ application/app/Http/Controllers/SeriesController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class SeriesController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function index()
+    {
+        return ['teste'];
+    }
+}
